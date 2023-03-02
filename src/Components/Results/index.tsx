cd87ae894ea0c4e0e49229fa784112421801b6a5
PROVIDA POWELL <PROVIDA_POWELL@YAHOO.COM>
2023-03-02 04:11:08 +0000

    more or less working

---
+++ src/Components/Results/index.tsx
@@ -0,0 +1,9 @@
+const Results = (props: { data: any; }) => {
+  return (
+    <section>
+      <pre>{props.data ? JSON.stringify(props.data, undefined, 2) : null}</pre>
+    </section>
+  );
+};
+
+export default Results;
